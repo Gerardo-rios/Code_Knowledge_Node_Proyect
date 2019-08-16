4c6dc6d37f3796a04ba572487c43cbfcd934eb25
ISRAEL LEON <israelpat42@gmail.com>
2019-08-16 22:42:40 +0000

    username en usuario

--- models/usuario.js
+++ models/usuario.js
@@ -1,6 +1,7 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
     const usuario = sequelize.define('usuario', {
+         username: {type:DataTypes.STRING, unique:true},
         nombres: DataTypes.STRING,
         external_id: DataTypes.UUID,
         apellidos: DataTypes.STRING,
